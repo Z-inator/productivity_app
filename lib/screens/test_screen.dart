de1955965c5d93b11ab5e38ed1d1ac39316b2307
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-02-11 13:50:45 -0600

    Building interface to test DateTime objects

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:productivity_app/components/task_page/task_list.dart';
@@ -36,12 +38,15 @@ class _TestScreenState extends State<TestScreen> {
   final _formKey = GlobalKey<FormState>();
   String _taskName;
   String _projectName;
-  DateTime _dueDate;
-  String _setDate;
-  String _setTime;
-  TextEditingController _dateController = TextEditingController();
-  TextEditingController _timeController = TextEditingController();
   List<dynamic> projectNames;
+
+  DateTime _dueDate = DateTime.now().toLocal();
+  TimeOfDay _dueTime = TimeOfDay.fromDateTime(DateTime.now().toLocal());
+  String year;
+  String month;
+  String day;
+  String hour;
+  String minute;
 
   @override
   Widget build(BuildContext context) {
@@ -53,169 +58,172 @@ class _TestScreenState extends State<TestScreen> {
               snapshot.docs.map((doc) => doc.data()['projectName']).toList());
     }
 
-    Future<void> selectDate(BuildContext context) async {
+    selectDate() async {
       final DateTime picked = await showDatePicker(
         context: context,
-        initialDate: DateTime.now(),
-        initialDatePickerMode: DatePickerMode.day,
+        initialDate: _dueDate,
         firstDate: DateTime(2020),
-        lastDate: DateTime(2021),
+        lastDate: DateTime(2100),
       );
       if (picked != null) {
         setState(() {
-          _setDate = picked.toString();
+          _dueDate = picked;
+          year = _dueDate.year.toString();
+          month = _dueDate.month.toString();
+          day = _dueDate.day.toString();
         });
       }
     }
 
-    Future<void> selectTime(BuildContext context) async {
-      final TimeOfDay picked =
-          await showTimePicker(context: context, initialTime: TimeOfDay.now());
-      if (picked != null) {
+    selectTime() async {
+      final TimeOfDay pickedTime =
+          await showTimePicker(context: context, initialTime: _dueTime);
+      if (pickedTime != null) {
         setState(() {
-          _setTime = picked.toString();
+          _dueTime = pickedTime;
+          hour = _dueTime.hour.toString();
+          minute = _dueTime.minute.toString();
+          _dueDate = DateTime.parse('$year-$month-$day $hour:$minute:00Z');
         });
       }
     }
 
     return Container(
-        child: SafeArea(
-            child: Scaffold(
-      body: Column(
-        children: [
-          RaisedButton(
-              onPressed: () {
-                AuthService()
-                    .signOut()
-                    .then((value) => print(FirebaseAuth.instance.currentUser));
-              },
-              child: Text('Sign Out')),
-          RaisedButton(
-              onPressed: () {
-                getProjectNames();
-                print(projectNames);
-                return showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('Add Task'),
-                        content: Form(
-                          key: _formKey,
-                          child: Column(
-                            children: <Widget>[
-                              TextFormField(
-                                validator: (value) => value.isEmpty
-                                    ? 'Please enter a task name'
-                                    : null,
-                                onChanged: (value) =>
-                                    setState(() => _taskName = value),
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: 'Task Name'),
-                              ),
-                              PopupMenuButton(onSelected: (value) {
-                                setState(() {
-                                  _projectName = value;
-                                });
-                              }, itemBuilder: (BuildContext context) {
-                                return projectNames
-                                    .map((name) => PopupMenuItem(
-                                        value: name, child: Text(name)))
-                                    .toList();
-                              }),
-                              // DropdownButtonFormField(
-                              //   value: _projectName,
-                              //   decoration: InputDecoration(
-                              //     border: OutlineInputBorder(),
-                              //     labelText: 'Project'
-                              //   ),
-                              //   items: projectNames.forEach((name) {
-                              //     return DropdownMenuItem(
-                              //       value: name,
-                              //       child: Text(name),
-                              //     );
-                              //   }),
-                              //   onChanged: (value) => setState(() => _projectName = value),
-                              // ),
-                              TextFormField(
-                                onTap: () {
-                                  selectDate(context);
-                                },
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: 'Due Date'),
-                              ),
-                              TextFormField(
-                                onTap: () {
-                                  selectTime(context);
-                                },
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: 'Due Time'),
-                              )
-                            ],
+      child: SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            RaisedButton(
+                onPressed: () {
+                  AuthService()
+                      .signOut()
+                      .then((value) => print(FirebaseAuth.instance.currentUser));
+                },
+                child: Text('Sign Out')),
+            RaisedButton(
+                onPressed: () {
+                  getProjectNames();
+                  print(projectNames);
+                  return showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Add Task'),
+                          content: Form(
+                            key: _formKey,
+                            child: Column(
+                              children: <Widget>[
+                                TextFormField(
+                                  validator: (value) => value.isEmpty
+                                      ? 'Please enter a task name'
+                                      : null,
+                                  onChanged: (value) =>
+                                      setState(() => _taskName = value),
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Task Name'),
+                                ),
+                                PopupMenuButton(onSelected: (value) {
+                                  setState(() {
+                                    _projectName = value;
+                                  });
+                                }, itemBuilder: (BuildContext context) {
+                                  return projectNames
+                                      .map((name) => PopupMenuItem(
+                                          value: name, child: Text(name)))
+                                      .toList();
+                                }),
+                                // DropdownButtonFormField(
+                                //   value: _projectName,
+                                //   decoration: InputDecoration(
+                                //     border: OutlineInputBorder(),
+                                //     labelText: 'Project'
+                                //   ),
+                                //   items: projectNames.forEach((name) {
+                                //     return DropdownMenuItem(
+                                //       value: name,
+                                //       child: Text(name),
+                                //     );
+                                //   }),
+                                //   onChanged: (value) => setState(() => _projectName = value),
+                                // ),
+                                TextButton(
+                                    onPressed: selectDate,
+                                    child: Text(
+                                        'Due Date: ${_dueDate.month}/${_dueDate.day}/${_dueDate.year}')),
+                                TextButton(
+                                    onPressed: selectTime,
+                                    child: Text(
+                                        'Due Time: ${_dueDate.hour}:${_dueDate.minute}')) // TODO: turn into Listtile to see if that updates
+                              ],
+                            ),
                           ),
-                        ),
-                        actions: [
-                          TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: Text('close'))
-                        ],
-                      );
-                    }
-                );
-              },
-              child: Text('Add Task')),
-          RaisedButton(
-              onPressed: () {
-                print(user.uid);
-                return showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return TasksStream(
-                        user: user,
-                      );
-                    });
-              },
-              child: Text('Show Tasks')),
-          RaisedButton(
-              onPressed: () {
-                print(user.uid);
-                return showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return ProjectsStream(
-                        user: user,
-                      );
-                    });
-              },
-              child: Text('Show Projects')),
-          RaisedButton(
-              onPressed: () {
-                print(user.uid);
-                return showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return TimeEntryStream(user: user);
-                    });
-              },
-              child: Text('Show Time Entries')),
-          RaisedButton(
-              onPressed: () {
-                print(user.uid);
-                return showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return TimerWidget(
-                        user: user,
-                      );
-                    });
-              },
-              child: Text('Show Timer')),
-        ],
-      ),
-    )));
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('close')),
+                            TextButton(
+                              onPressed: () => print(_dueDate.toString()),
+                                // onPressed: () => TaskService(user: user).addTask(
+                                //     taskName: _taskName,
+                                //     projectName: _projectName,
+                                //     dueDate: _dueDate),
+                                child: Text('Submit'))
+                          ],
+                        );
+                      });
+                },
+                child: Text('Add Task')),
+            RaisedButton(
+                onPressed: () {
+                  print(user.uid);
+                  return showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return TasksStream(
+                          user: user,
+                        );
+                      });
+                },
+                child: Text('Show Tasks')),
+            RaisedButton(
+                onPressed: () {
+                  print(user.uid);
+                  return showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return ProjectsStream(
+                          user: user,
+                        );
+                      });
+                },
+                child: Text('Show Projects')),
+            RaisedButton(
+                onPressed: () {
+                  print(user.uid);
+                  return showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return TimeEntryStream(user: user);
+                      });
+                },
+                child: Text('Show Time Entries')),
+            RaisedButton(
+                onPressed: () {
+                  print(user.uid);
+                  return showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return TimerWidget(
+                          user: user,
+                        );
+                      });
+                },
+                child: Text('Show Timer')),
+          ],
+        ),
+      )));
   }
 }
