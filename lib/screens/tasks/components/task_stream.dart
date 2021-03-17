dc855439c7d75fe47f3fe22abc78994649b2bc41
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-03-17 17:27:23 -0500

    Working on Filter views

--- lib/screens/tasks/components/task_stream.dart
+++ lib/screens/tasks/components/task_stream.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:productivity_app/screens/tasks/components/task_project_future.dart';
 import 'package:productivity_app/screens/tasks/components/task_status_future.dart';
@@ -22,17 +23,12 @@ class _TaskStreamState extends State<TaskStream>
   @override
   bool get wantKeepAlive => true;
 
-  int selectedPage = 0;
+  int selectedPage;
   Widget body;
 
-  List<Widget> pages = [
-    TaskStatusesFuture(), // TODO: fix this function call
-    TaskProjectsFuture(),
-  ];
-
-  void setPage(int index, AsyncSnapshot<QuerySnapshot> querySnapshot) {
+  _updateBody(int index, AsyncSnapshot<QuerySnapshot> querySnapshot) {    // TODO: try using Provider to set a changeNotifier and Listener
     setState(() {
-      switch (selectedPage) {
+      switch (index) {
         case 0:
           body = TaskStatusesFuture(querySnapshot: querySnapshot);
           break;
@@ -67,111 +63,138 @@ class _TaskStreamState extends State<TaskStream>
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                StatefulBuilder(builder:
-                    (BuildContext context, StateSetter filterSetState) {
-                  return SingleChildScrollView(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: TextButton.icon(
-                            onPressed: () {
-                              filterSetState(() {
-                                selectedPage = 0;
-                                body = TaskStatusesFuture(querySnapshot: snapshot);
-                              });
-                              // setPage(0, snapshot);
-                            },
-                            icon: Icon(Icons.label_rounded),
-                            label: Text('Status'),
-                            style: TextButton.styleFrom(
-                              backgroundColor: selectedPage == 0
-                                  ? Theme.of(context).primaryColor
-                                  : null,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(25))),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: TextButton.icon(
-                            onPressed: () {
-                              filterSetState(() {
-                                selectedPage = 1;
-                                body = TaskStatusesFuture(querySnapshot: snapshot);
-                              });
-                              // setPage(1, snapshot);
-                            },
-                            icon: Icon(Icons.notification_important_rounded),
-                            label: Text('Due Date'),
-                            style: TextButton.styleFrom(
-                              backgroundColor: selectedPage == 1
-                                  ? Theme.of(context).primaryColor
-                                  : null,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(25))),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: TextButton.icon(
-                            onPressed: () {
-                              filterSetState(() {
-                                selectedPage = 2;
-                                body = TaskStatusesFuture(querySnapshot: snapshot);
-                              });
-                              // setPage(2, snapshot);
-                            },
-                            icon: Icon(Icons.playlist_add_rounded),
-                            label: Text('Create Date'),
-                            style: TextButton.styleFrom(
-                              backgroundColor: selectedPage == 2
-                                  ? Theme.of(context).primaryColor
-                                  : null,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(25))),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: TextButton.icon(
-                            onPressed: () {
-                              filterSetState(() {
-                                selectedPage = 3;
-                                body = TaskProjectsFuture(querySnapshot: snapshot);
-                              });
-                              // setPage(3, snapshot);
-                            },
-                            icon: Icon(Icons.topic_rounded),
-                            label: Text('Project'),
-                            style: TextButton.styleFrom(
-                              backgroundColor: selectedPage == 3
-                                  ? Theme.of(context).primaryColor
-                                  : null,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(25))),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
+                FilterButtonRow(setBody: setBody),
                 Expanded(
                     child: body ?? TaskStatusesFuture(querySnapshot: snapshot))
               ],
             ),
           );
         });
+  }
+}
+
+class FilterButtonRow extends StatefulWidget {
+  const FilterButtonRow({Key key, @required this.setBody}) : super(key: key);
+
+  final Function setBody;
+
+  @override
+  _FilterButtonRowState createState() => _FilterButtonRowState();
+}
+
+class _FilterButtonRowState extends State<FilterButtonRow> {
+  int selectedPage;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  selectedPage == 0;
+                });
+                widget.setBody(selectedPage);
+              },
+              icon: Icon(Icons.label_rounded),
+              label: Text('Status'),
+              style: TextButton.styleFrom(
+                backgroundColor:
+                    selectedPage == 0 ? Theme.of(context).primaryColor : null,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25))),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  widget.selectedPage == 1;
+                });
+              },
+              icon: Icon(Icons.notification_important_rounded),
+              label: Text('Due Date'),
+              style: TextButton.styleFrom(
+                backgroundColor: widget.selectedPage == 1
+                    ? Theme.of(context).primaryColor
+                    : null,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25))),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  widget.selectedPage == 2;
+                });
+              },
+              icon: Icon(Icons.playlist_add_rounded),
+              label: Text('Create Date'),
+              style: TextButton.styleFrom(
+                backgroundColor: widget.selectedPage == 2
+                    ? Theme.of(context).primaryColor
+                    : null,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25))),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  widget.selectedPage == 3;
+                });
+              },
+              icon: Icon(Icons.topic_rounded),
+              label: Text('Project'),
+              style: TextButton.styleFrom(
+                backgroundColor: widget.selectedPage == 3
+                    ? Theme.of(context).primaryColor
+                    : null,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25))),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TaskBodyWidget extends StatelessWidget {
+  void setPage(int index, AsyncSnapshot<QuerySnapshot> querySnapshot) {
+    switch (index) {
+      case 0:
+        body = TaskStatusesFuture(querySnapshot: querySnapshot);
+        break;
+      case 1:
+        body = TaskStatusesFuture(querySnapshot: querySnapshot);
+        break;
+      case 2:
+        body = TaskStatusesFuture(querySnapshot: querySnapshot);
+        break;
+      case 3:
+        body = TaskProjectsFuture(querySnapshot: querySnapshot);
+        break;
+      default:
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
