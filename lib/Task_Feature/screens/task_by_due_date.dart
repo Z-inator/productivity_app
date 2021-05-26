0ae9dca45885feca5b8cd699ac6b1d01c6f3ecee
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-05-26 12:07:49 -0500

    In the process of removing these files/widgets for a more reusable approach.

--- lib/Task_Feature/screens/task_by_due_date.dart
+++ lib/Task_Feature/screens/task_by_due_date.dart
@@ -1,61 +1,61 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dynamic_color_theme/dynamic_color_theme.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:productivity_app/Task_Feature/models/projects.dart';
-import 'package:productivity_app/Task_Feature/models/tasks.dart';
-import 'package:productivity_app/Task_Feature/screens/components/grouped_tasks.dart';
-import 'package:productivity_app/Task_Feature/screens/components/project_edit_bottomsheet.dart';
-import 'package:productivity_app/Task_Feature/screens/components/task_edit_bottomsheet.dart';
-import 'package:productivity_app/Task_Feature/screens/components/task_expansion_tile.dart';
-import 'package:productivity_app/Task_Feature/screens/task_screen.dart';
-import 'package:productivity_app/Task_Feature/services/projects_data.dart';
-import 'package:productivity_app/Task_Feature/services/projects_data.dart';
-import 'package:productivity_app/Task_Feature/services/tasks_data.dart';
-import 'package:productivity_app/Time_Feature/models/times.dart';
-import 'package:productivity_app/Time_Feature/services/times_data.dart';
-import 'package:productivity_app/Shared/functions/color_functions.dart';
-import 'package:productivity_app/Shared/functions/datetime_functions.dart';
-import 'package:productivity_app/Shared/functions/time_functions.dart';
-import 'package:provider/provider.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:dynamic_color_theme/dynamic_color_theme.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:productivity_app/Task_Feature/models/projects.dart';
+// import 'package:productivity_app/Task_Feature/models/tasks.dart';
+// import 'package:productivity_app/Task_Feature/screens/components/grouped_tasks.dart';
+// import 'package:productivity_app/Task_Feature/screens/components/project_edit_bottomsheet.dart';
+// import 'package:productivity_app/Task_Feature/screens/components/task_edit_bottomsheet.dart';
+// import 'package:productivity_app/Task_Feature/screens/components/task_expansion_tile.dart';
+// import 'package:productivity_app/Task_Feature/screens/task_screen.dart';
+// import 'package:productivity_app/Task_Feature/services/projects_data.dart';
+// import 'package:productivity_app/Task_Feature/services/projects_data.dart';
+// import 'package:productivity_app/Task_Feature/services/tasks_data.dart';
+// import 'package:productivity_app/Time_Feature/models/times.dart';
+// import 'package:productivity_app/Time_Feature/services/times_data.dart';
+// import 'package:productivity_app/Shared/functions/color_functions.dart';
+// import 'package:productivity_app/Shared/functions/datetime_functions.dart';
+// import 'package:productivity_app/Shared/functions/time_functions.dart';
+// import 'package:provider/provider.dart';
 
-class TaskByDueDate extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    TaskService taskService = Provider.of<TaskService>(context);
-    final List<Task> tasks = Provider.of<List<Task>>(context);
-    List<DateTime> days = taskService.getDays(tasks, true);
-    return days == null
-        ? Center(child: CircularProgressIndicator())
-        : days.isEmpty
-            ? Center(child: Text('There are no Tasks with a due date.'))
-            : ListView(
-                padding: EdgeInsets.only(bottom: 100),
-                children: days.map((day) {
-                  List<Task> associatedTasks =
-                      taskService.getTasksByDate(tasks, day, true);
-                  return Container(
-                    padding: EdgeInsets.all(10),
-                    child: Card(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ListTile(
-                            title: Text(DateTimeFunctions()
-                                .dateTimeToTextDate(date: day)),
-                            trailing: Text(associatedTasks.length.toString(),
-                                style: DynamicColorTheme.of(context)
-                                    .data
-                                    .textTheme
-                                    .subtitle1),
-                          ),
-                          GroupedTasks(associatedTasks: associatedTasks)
-                        ],
-                      ),
-                    ),
-                  );
-                }).toList(),
-              );
-  }
-}
+// class TaskByDueDate extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     TaskService taskService = Provider.of<TaskService>(context);
+//     final List<Task> tasks = Provider.of<List<Task>>(context);
+//     List<DateTime> days = taskService.getDays(tasks, true);
+//     return days == null
+//         ? Center(child: CircularProgressIndicator())
+//         : days.isEmpty
+//             ? Center(child: Text('There are no Tasks with a due date.'))
+//             : ListView(
+//                 padding: EdgeInsets.only(bottom: 100),
+//                 children: days.map((day) {
+//                   List<Task> associatedTasks =
+//                       taskService.getTasksByDate(tasks, day, true);
+//                   return Container(
+//                     padding: EdgeInsets.all(10),
+//                     child: Card(
+//                       child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                         mainAxisSize: MainAxisSize.min,
+//                         children: [
+//                           ListTile(
+//                             title: Text(DateTimeFunctions()
+//                                 .dateTimeToTextDate(date: day)),
+//                             trailing: Text(associatedTasks.length.toString(),
+//                                 style: DynamicColorTheme.of(context)
+//                                     .data
+//                                     .textTheme
+//                                     .subtitle1),
+//                           ),
+//                           GroupedTasks(tasks: associatedTasks)
+//                         ],
+//                       ),
+//                     ),
+//                   );
+//                 }).toList(),
+//               );
+//   }
+// }
