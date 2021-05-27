9b9a880e5f129a60b9f446c5febbe911cb509eee
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-05-27 16:07:00 -0500

    Moved ProjectPicker to a more appropriate file location inside of Tasks Feature

---
+++ lib/Task_Feature/screens/components/project_picker.dart
@@ -0,0 +1,66 @@
+import 'package:dynamic_color_theme/dynamic_color_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:productivity_app/Task_Feature/models/projects.dart';
+import 'package:productivity_app/Task_Feature/providers/task_edit_state.dart';
+import 'package:provider/provider.dart';
+
+class ProjectPicker extends StatelessWidget {
+  final Function(Project) saveProject;
+  final Function noProject;
+  final Widget child;
+  const ProjectPicker({Key key, this.saveProject, this.noProject, this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Project> projects = Provider.of<List<Project>>(context);
+    return PopupMenuButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))),
+        itemBuilder: (BuildContext context) {
+          return <PopupMenuEntry>[
+            PopupMenuItem(
+                child: ListTile(
+              leading: Icon(
+                Icons.topic_rounded,
+                color: Color(4285887861),
+              ),
+              title: Text('No Project',
+                  style:
+                      DynamicColorTheme.of(context).data.textTheme.subtitle1),
+              onTap: () {
+                saveProject(Project());
+                Navigator.pop(context);
+              },
+            )),
+            PopupMenuDivider(),
+            PopupMenuItem(
+              child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(
+                  children: projects.map((project) {
+                    return ListTile(
+                      leading: Icon(
+                        Icons.topic_rounded,
+                        color: Color(project.projectColor),
+                      ),
+                      title: Text(project.projectName,
+                          style: DynamicColorTheme.of(context)
+                              .data
+                              .textTheme
+                              .subtitle1),
+                      onTap: () {
+                        saveProject(project);
+                        Navigator.pop(context);
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+            )
+          ];
+        },
+        child: child);
+  }
+}
